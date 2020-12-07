1fd20b701eb3a32bf88a0f225bf22682a62e5091
ysr <yongsr12345@gmail.com>
2020-12-07 16:05:34 +0800

    FIx port problem

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
